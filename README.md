94d1d65226e00904fee26d5302505547d85bc3de
Ariel201711 <arielk216@gmail.com>
2022-11-17 17:43:52 +0200

    organizing readme title and replacing mister-bitcoin occurences to super-bitcoin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SuperBitcoin
+# Super Bitcoin
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.6.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mister-bitcoin'),
+      dir: require('path').join(__dirname, './coverage/super-bitcoin'),
       subdir: '.',
       reporters: [
         { type: 'html' },
